c34ad6273fc9a1f7f4f51323ac00038ee774dde9
hassan629 <hsnh83099.com>
2023-10-17 21:55:31 +0200

    done

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include "main.h"
 int main(void)
 {
-<<<<<<< HEAD
-	_printf("Hello, %s!\n", "world");
-	return (0);
-=======
-    _printf("Hello, %s!\n", "world");
-    _printf("The number is: %d\n", 12345);
-    _printf("The negative number is: %d\n", -9876);
+< << << << HEAD
+_printf("Hello, %s!\n", "world");
+return (0);
+= == == ==
+_printf("Hello, %s!\n", "world");
+_printf("The number is: %d\n", 12345);
+_printf("The negative number is: %d\n", -9876);
 
-    return 0;
->>>>>>> 610ec24927d5a42b13e022534130e7d5d8a30b5d
+return (0);
+> >> >> >> 610ec24927d5a42b13e022534130e7d5d8a30b5d
 }
