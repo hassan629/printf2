610ec24927d5a42b13e022534130e7d5d8a30b5d
Tayba-m <yanoomohamed@gmial.com>
2023-10-17 10:57:59 -0400

    commit last

--- _print.c
+++ _print.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /** _printf - Prints formatted output to the standard output.
  * @format: The format string specifying the output format.
@@ -11,7 +12,7 @@ int _puts(const char *str);
 
 int _printf(const char *format, ...)
 {
-    va_list args;
+	 va_list args;
     int printed_chars = 0;
 
     va_start(args, format);
@@ -20,41 +21,75 @@ int _printf(const char *format, ...)
     {
         if (*format == '%')
         {
-            format++; /* Move past the '%'*/
+            format++; /*Move past the '%'*/
 
-            /* Handle the conversion specifier*/
+            /*Handle the conversion specifier*/
             switch (*format)
             {
-            case 'c':
-            {
-                /*Print a character*/
-                char c = (char)va_arg(args, int);
-                printed_chars += _putchar(c);
-                break;
-            }
-            case 's':
-            {
-                /* Print a string*/
-                char *str = va_arg(args, char *);
-                printed_chars += _puts(str);
-                break;
-            }
-            case '%':
-            {
-                /* Print a percent sign*/
-                printed_chars += _putchar('%');
-                break;
-            }
-            default:
-                /* Invalid conversion specifier, ignore and continue*/
-                printed_chars += _putchar('%');
-                printed_chars += _putchar(*format);
-                break;
-            }
+            	case 'c':
+            	{
+                	/*Print a character*/
+                	char c = (char)va_arg(args, int);
+                	printed_chars += _putchar(c);
+                	break;
+            	}
+            	case 's':
+            	{
+                	/*Print a string*/
+                	char *str = va_arg(args, char *);
+                	printed_chars += _puts(str);
+                	break;
+            	}
+            	case 'd':
+            	case 'i':
+            	{	
+                	/*Print a signed decimal integer*/
+                	int num = va_arg(args, int);
+                	char buffer[20];
+                	int t = 0, s;
+
+                	if (num < 0)
+                	{
+                    		printed_chars += _putchar('-');
+                    		num = -num;
+                	}
+
+                	if (num == 0)
+                	{	
+                    		printed_chars += _putchar('0');
+                	}
+                	else
+                	{
+                    		while (num != 0)
+                    		{	
+                        		buffer[t++] = num % 10 + '0';
+                        		num /= 10;
+                    		}
+
+                    	for (s = t - 1; s >= 0; s--)
+			{
+                        	printed_chars += _putchar(buffer[s]);
+			}
+                	}
+
+                	break;
+            	}
+            	case '%':
+            	{
+                	/*Print a percent sign*/
+                	printed_chars += _putchar('%');
+                	break;
+            	}
+            		default:
+                	/*Invalid conversion specifier, ignore and continue*/
+                	printed_chars += _putchar('%');
+                	printed_chars += _putchar(*format);
+                	break;
+	    }
         }
         else
         {
-            /* Regular character, print as-is*/
+            /*Regular character, print as-is*/
             printed_chars += _putchar(*format);
         }
 
--- main.c
+++ main.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
 #include "main.h"
 
 int main()
 {
     _printf("Hello, %s!\n", "world");
+    _printf("The number is: %d\n", 12345);
+    _printf("The negative number is: %d\n", -9876);
+
     return 0;
 }
